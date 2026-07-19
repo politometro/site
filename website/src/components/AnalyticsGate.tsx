8061e14812411a364b798047dea13124af38db24
MaNmAxIm0 <luis_maximo@iscte-iul.pt>
2026-07-19 12:07:26 +0100

    Add mobile analytics opt-out link

--- website/src/components/AnalyticsGate.tsx
+++ website/src/components/AnalyticsGate.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import { Analytics, type BeforeSendEvent } from "@vercel/analytics/next";
+import { useEffect } from "react";
 
 export default function AnalyticsGate() {
+  useEffect(() => {
+    if (new URLSearchParams(window.location.search).get("analytics") === "off") {
+      window.localStorage.setItem("va-disable", "true");
+    }
+    if (new URLSearchParams(window.location.search).get("analytics") === "on") {
+      window.localStorage.removeItem("va-disable");
+    }
+  }, []);
+
   return (
     <Analytics
       beforeSend={(event: BeforeSendEvent) => {
         try {
-          if (window.localStorage.getItem("va-disable") === "true") {
+          const queryDisabled =
+            new URLSearchParams(window.location.search).get("analytics") === "off";
+          if (queryDisabled || window.localStorage.getItem("va-disable") === "true") {
             return null;
           }
         } catch {
